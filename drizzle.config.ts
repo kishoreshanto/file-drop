9cd2a9a15d32c7643e1f9f2155e9f66e87ad9d6d
Kishore Shanto <tinykishore@icloud.com>
2026-05-27 23:22:34 +0600

    feat: add drizzle configuration for database schema and migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+	schema: './src/lib/server/db/schema.ts',
+	out: './src/lib/server/db/migrations',
+	dialect: 'sqlite',
+	dbCredentials: {
+		url: './.local-data/app/file-drop.db'
+	}
+});
